e6d37053e93fddfad2fc867b9a80bf99978ee946
kdalkafoukis <kdalkafoukis@yahoo.gr>
2024-04-04 15:56:39 +0100

    create the actual random walk for one step

--- random-walks-on-the-line.py
+++ random-walks-on-the-line.py
@@ -13,6 +13,8 @@ U1 = np.array([[0, 0, 0, 0, 0, 0, 0, 1],
                [0, 0, 0, 0, 0, 1, 0, 0],
                [0, 0, 0, 0, 0, 0, 1, 0]], dtype=int)
 
+U1 = Operator(U1)
+
 U2 = np.array([[0, 1, 0, 0, 0, 0, 0, 0],
                [0, 0, 1, 0, 0, 0, 0, 0],
                [0, 0, 0, 1, 0, 0, 0, 0],
@@ -22,25 +24,36 @@ U2 = np.array([[0, 1, 0, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 0, 0, 1],
                [1, 0, 0, 0, 0, 0, 0, 0]], dtype=int)
 
+U2 = Operator(U2)
 
-U1 = Operator(U1)
-U2 = Operator(U1)
-
-qr = QuantumRegister(3)
-cr = ClassicalRegister(3)
+cr = ClassicalRegister(4)
+qr = QuantumRegister(4)
 qc = QuantumCircuit(qr, cr)
 
-# qc.x(0)
+
+qc.h(0)
 # qc.x(1)
 # qc.x(2)
+# qc.x(3)
 
-qc.append(U1, [0, 1, 2])
-# qc.append(U2, [0, 1, 2])
+U1_op = U1.to_instruction()
+U1_controlled = U1_op.control(1)
+qc.append(U1_controlled, [0, 1, 2, 3])
 
-qc.measure([0, 1, 2], [0, 1, 2])
 
-# qc.draw('mpl')
+U2_op = U2.to_instruction()
+U2_controlled = U2_op.control(1, ctrl_state=0)
+qc.append(U2_controlled, [0, 1, 2, 3])
 
+
+qc.h(0)
+qc.append(U1_controlled, [0, 1, 2, 3])
+qc.append(U2_controlled, [0, 1, 2, 3])
+
+
+qc.measure([0, 1, 2, 3], [3, 0, 1, 2])
+
+qc.draw('mpl')
 backend = BasicSimulator()
 tqc = transpile(qc, backend)
 counts = backend.run(tqc).result().get_counts()
